6dc7787a76814074da1a7624b507e7b08d1058b3
@h0lycr4p <blueeyedagain@gmail.com>
2025-06-03 17:27:44 +0200

    fixed leaving game logic

--- backend/generated/prisma/index.js
+++ backend/generated/prisma/index.js
@@ -35,12 +35,12 @@ exports.Prisma = Prisma
 exports.$Enums = {}
 
 /**
- * Prisma Client JS version: 6.7.0
- * Query Engine version: 3cff47a7f5d65c3ea74883f1d736e41d68ce91ed
+ * Prisma Client JS version: 6.8.2
+ * Query Engine version: 2060c79ba17c6bb9f5823312b6f6b7f4a845738e
  */
 Prisma.prismaVersion = {
-  client: "6.7.0",
-  engine: "3cff47a7f5d65c3ea74883f1d736e41d68ce91ed"
+  client: "6.8.2",
+  engine: "2060c79ba17c6bb9f5823312b6f6b7f4a845738e"
 }
 
 Prisma.PrismaClientKnownRequestError = PrismaClientKnownRequestError;
@@ -165,7 +165,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "D:\\SUMMER 2025 POLIBUDA\\Advanced Web Tech\\pict-io\\backend\\generated\\prisma",
+      "value": "C:\\Users\\bluee\\Documents\\PWR\\Sem6\\pict-io-miki\\pict-io\\backend\\generated\\prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -179,7 +179,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "D:\\SUMMER 2025 POLIBUDA\\Advanced Web Tech\\pict-io\\backend\\prisma\\schema.prisma",
+    "sourceFilePath": "C:\\Users\\bluee\\Documents\\PWR\\Sem6\\pict-io-miki\\pict-io\\backend\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
@@ -187,8 +187,8 @@ const config = {
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
-  "clientVersion": "6.7.0",
-  "engineVersion": "3cff47a7f5d65c3ea74883f1d736e41d68ce91ed",
+  "clientVersion": "6.8.2",
+  "engineVersion": "2060c79ba17c6bb9f5823312b6f6b7f4a845738e",
   "datasourceNames": [
     "db"
   ],
@@ -197,7 +197,7 @@ const config = {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:pass@localhost:5432/pictio"
+        "value": null
       }
     }
   },
